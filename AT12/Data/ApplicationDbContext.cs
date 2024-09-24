95b25aef5e271a446316915cec8ac0eb22eb59ce
Doherty <gabriel.dcarvalho@al.infnet.edu.br>
2024-09-24 20:26:40 -0300

    Adicionar arquivos de projeto.

---
+++ AT12/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AT12.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            if (Database.IsSqlite())
+            {
+                // Ajusta o tamanho das colunas para o SQLite
+                foreach (var entity in builder.Model.GetEntityTypes())
+                {
+                    foreach (var property in entity.GetProperties())
+                    {
+                        if (property.ClrType == typeof(string) && property.GetMaxLength() == null)
+                        {
+                            property.SetColumnType("TEXT");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ AT12/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AT12.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Nome { get; set; }
+        public DateTime DataNasc { get; set; }
+    }
+}
